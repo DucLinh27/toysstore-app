30f0e2a3ae1a10c3574f23f8362cf09ce9ecdd5a
Tran Duc Linh <duclinhlmht92@gmail.com>
2022-10-27 10:52:08 +0700

    new Commit

--- models/pg_config.js
+++ models/pg_config.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 const pg_conn = new Pool (
     {
-        user:'harseggtektufs',
-        password:'652952652f0a4b73ff536d5ee0711e9f92b7f57353904f75a130d95b4faac223',
-        host:'ec2-44-195-100-240.compute-1.amazonaws.com',
-        database:'d4bs0p4r9f27gm',
+        user:'ycqyvabesibdmp',
+        password:'3e74f21a14386878b9d9f05c7b72fd172d324091789f3ed592736187ceb028b0',
+        host:'ec2-54-147-36-107.compute-1.amazonaws.com',
+        database:'dfr2r7u48k4aq6',
         port:5432,
         ssl: {
             rejectUnauthorized: false
